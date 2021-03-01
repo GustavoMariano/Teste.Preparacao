d60bbd195cb1825a585f44d8c659581e5b9a5c13
GUSTAVO CESAR MARIANO <gustavocmariano@hotmail.com>
2021-03-01 14:25:27 -0300

    Add project files.

---
+++ Teste.Preparacao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.Preparacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
