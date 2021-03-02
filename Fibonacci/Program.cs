e2caa162e4d8e37b8c341a8f2c54c648c566f814
GUSTAVO CESAR MARIANO <gustavocmariano@hotmail.com>
2021-03-02 00:11:24 -0300

    Fibonacci e principais operadores aritmeticos.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main()
+        {            
+            int num1 = 1, num2 = 0, resultado = 0; //x y z
+
+            int quantidade; //qtd para for
+            quantidade = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                resultado = num1 + num2;
+                Console.WriteLine(resultado);
+                num1 = num2;
+                num2 = resultado;
+            }
+        }
+    }
+}
---
+++ Principais.Operacoes.Aritmeticas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Principais.Operacoes.Aritmeticas
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1, num2;
+
+            Console.WriteLine("Insira o primeiro numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Adição: " + (num1 + num2));
+            Console.WriteLine("Subtração: " + (num1 - num2));
+            Console.WriteLine("Multiplicação: " + (num1 * num2));
+            Console.WriteLine("Divisão: " + (num1 / num2));
+
+            if(num1 % 2 == 0)
+            {
+                Console.WriteLine(num1 + " É par");
+            }
+            else
+            {
+                Console.WriteLine(num1 + " É impar");
+            }
+
+            if (num2 % 2 == 0)
+            {
+                Console.WriteLine(num2 + " É par");
+            }
+            else
+            {
+                Console.WriteLine(num2 + " É impar");
+            }
+        }
+    }
+}
